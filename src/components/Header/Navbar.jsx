bd27ad952a5f6e34b468bec7da865ff170647375
Big-Smokkee <fardinfaraz13579@gmail.com>
2026-04-15 23:35:50 +0600

    feat: fix the hover effect of the navbar

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 
 const Navbar = () => {
-    const links = <div className='flex gap-3'>
-        <li className='hover:border border-green-400 p-2 rounded-xl'>Home</li>
-        <li className='hover:border border-green-400 p-2 rounded-xl'>Listed Books</li>
-        <li className='hover:border border-green-400 p-2 rounded-xl'>Pages to read</li>
+    const links = <div className='block md:flex gap-3'>
+        <li className='p-2 hover:border border-green-400  rounded-xl'>Home</li>
+        <li className='p-2 hover:border border-green-400  rounded-xl'>Listed Books</li>
+        <li className='p-2 hover:border border-green-400  rounded-xl'>Pages to read</li>
     </div>
     return (
         <div className="navbar  shadow-sm px-32.5 text-black work">
-            <div className="navbar-start">
-                <div className="dropdown">
+            <div className="navbar-start ">
+                <div className="dropdown bg-white">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
                     </div>
                     <ul
                         tabIndex="-1"
-                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                        className="menu menu-sm dropdown-content bg-white rounded-box z-1 mt-3 w-52 p-2 shadow">
                         {links}
                     </ul>
                 </div>
