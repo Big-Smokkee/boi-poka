815d572b4e4be331888d9bc25a7318052b48062c
Big-Smokkee <fardinfaraz13579@gmail.com>
2026-04-15 20:12:37 +0600

    feat: move router defination from the main.jsx so that it can be structered

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { createBrowserRouter } from "react-router";
+import App from '../App';
+import Root from '../pages/Root/Root';
+import ErrorPage from '../pages/ErrorPage/ErrorPage';
+import Home from '../pages/Home/Home';
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root></Root>,
+        errorElement: <ErrorPage></ErrorPage>,
+        children: [
+            {
+                index: true,
+                path: '/',
+                Component: Home
+            }
+        ]
+    },
+]);
