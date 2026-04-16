f9ec81514cbd12a5f604721f94fae2711d7c5c18
Big-Smokkee <fardinfaraz13579@gmail.com>
2026-04-16 18:10:36 +0600

    showed book details

--- src/pages/BookDetails/BookDetails.jsx
+++ src/pages/BookDetails/BookDetails.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { useLoaderData, useParams } from 'react-router';
 
 const BookDetails = () => {
+    const { id } = useParams();
+    console.log(id);
+    const bookId = parseInt(id)
+    const data = useLoaderData();
+    const singleBook = data.find(book => book.bookId === bookId)
+    console.log(singleBook);
+    const { bookName, image } = singleBook;
     return (
-        <div>
-            <h2>Ami book details</h2>
+        <div className=' w-2/3 mx-auto'>
+            <img src={image} alt="" className='w-48' />
+            <h5>{bookName}</h5>
+            <button className="btn btn-accent m-2">Read</button>
+            <button className="btn btn-info m-2">Wishlist</button>
         </div>
+
     );
 };
 
